d9e8399a5118a7d55fbe90a585c34eca667db49e
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-09 14:35:23 -0600

    initial commit - project initialized

---
+++ fetchchallenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  fetchchallenge
+//
+//  Created by octopus on 6/8/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ fetchchallenge/fetchchallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  fetchchallengeApp.swift
+//  fetchchallenge
+//
+//  Created by octopus on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct fetchchallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
