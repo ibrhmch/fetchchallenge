5ea96944fa67a5332a424e6c0ef68500fe911cc5
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-09 20:10:23 -0600

    centered the navigationBarTitle and rendering a search bar

--- fetchchallenge/Views/MealListView.swift
+++ fetchchallenge/Views/MealListView.swift
@@ -8,11 +8,35 @@
 import SwiftUI
 
 struct MealListView: View {
+    @State private var searchText = ""
     @StateObject private var viewModel = MealListViewModel()
     
     var body: some View {
         NavigationView {
             VStack {
+                HStack {
+                    HStack {
+                        Image(systemName: "magnifyingglass")
+                        TextField("Search", text: $searchText)
+                    }.underlineTextField()
+                    
+                    if !searchText.isEmpty {
+                        Button("Cancel") {
+                            searchText = ""
+                        }
+                        .foregroundColor(Color.primary)
+                        .padding(.vertical, 6)
+                        .padding(.horizontal, 7)
+                        .background(.fill)
+                        .cornerRadius(7)
+                        
+                        Spacer()
+                    }
+                }
+                .padding()
+                .background(Color(UIColor.systemBackground))
+                
+                
                 if viewModel.isLoading {
                     ProgressView("Loading meals...")
                 } else if let errorMessage = viewModel.errorMessage {
@@ -53,10 +77,11 @@ struct MealListView: View {
                     }
                 }
             }
-            .navigationTitle("Dessert Meals")
+            .navigationBarTitle("Dessert Meals", displayMode: .inline)
             .task {
                 await viewModel.fetchMeals()
             }
+            
         }
     }
 }
