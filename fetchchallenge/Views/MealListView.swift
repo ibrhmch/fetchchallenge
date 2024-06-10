3107bd6378340ada5f09271e7d36374c0ea551df
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-09 19:08:50 -0600

    create View to show a list of meals of dessert

--- fetchchallenge/Views/MealListView.swift
+++ fetchchallenge/Views/MealListView.swift
@@ -8,8 +8,31 @@
 import SwiftUI
 
 struct MealListView: View {
+    @StateObject private var viewModel = MealListViewModel()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            VStack {
+                if viewModel.isLoading {
+                    ProgressView("Loading meals...")
+                } else if let errorMessage = viewModel.errorMessage {
+                    Text("Error: \(errorMessage)")
+                } else {
+                    List(viewModel.meals) { meal in
+                        HStack {
+                            AsyncImage(url: URL(string: meal.thumbnail))
+                                .frame(width: 50, height: 50)
+                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                            Text(meal.name)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Dessert Meals")
+            .task {
+                await viewModel.fetchMeals()
+            }
+        }
     }
 }
 
