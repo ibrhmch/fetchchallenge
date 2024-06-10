8a042fe49061e5bab8c13e712fe5f06f1e5a8ff4
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-09 19:00:03 -0600

    model to store API response of list of meals

---
+++ fetchchallenge/Models/MealResponse.swift
@@ -0,0 +1,12 @@
+//
+//  MealResponse.swift
+//  fetchchallenge
+//
+//  Created by octopus on 6/9/24.
+//
+
+import Foundation
+
+struct MealResponse: Codable {
+    let meals: [Meal]
+}
---
+++ fetchchallenge/Views/MealListView.swift
@@ -0,0 +1,18 @@
+//
+//  MealListView.swift
+//  fetchchallenge
+//
+//  Created by octopus on 6/9/24.
+//
+
+import SwiftUI
+
+struct MealListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealListView()
+}
