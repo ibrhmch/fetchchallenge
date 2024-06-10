586c5152f33e66cdfeff145155fc2c961300fc5e
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-10 00:22:39 -0600

    created a view to display mealdetails

--- fetchchallenge/Views/MealDetailView.swift
+++ fetchchallenge/Views/MealDetailView.swift
@@ -8,11 +8,43 @@
 import SwiftUI
 
 struct MealDetailView: View {
+    @StateObject private var viewModel = MealDetailViewModel()
+    let mealID: String
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            if viewModel.isLoading {
+                ProgressView("Loading meal details...")
+            } else if let errorMessage = viewModel.errorMessage {
+                Text("Error: \(errorMessage)")
+            } else if let mealDetail = viewModel.mealDetail {
+                VStack(alignment: .leading, spacing: 16) {
+                    Text(mealDetail.name)
+                        .font(.largeTitle)
+                        .bold()
+                    Text("Instructions")
+                        .font(.headline)
+                    Text(mealDetail.instructions)
+                    Text("Ingredients")
+                        .font(.headline)
+                    ForEach(Array(zip(mealDetail.ingredients, mealDetail.measurements)), id: \.0) { ingredient, measurement in
+                        HStack {
+                            Text(ingredient)
+                            Spacer()
+                            Text(measurement)
+                        }
+                    }
+                }
+                .padding()
+            }
+        }
+        .navigationBarTitle("Meal Details", displayMode: .inline)
+        .task {
+            await viewModel.fetchMealDetail(mealID: mealID)
+        }
     }
 }
 
 #Preview {
-    MealDetailView()
+    MealDetailView(mealID: "53049")
 }
