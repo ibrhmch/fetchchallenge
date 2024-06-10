e3e55320a7c254836d839b81d8bf1a328a589a4a
Ch Mohammad Ibrahim <ibihaxor@gmail.com>
2024-06-09 19:55:44 -0600

    Changed the Hstack to Zstack to render the image as background and name of the meal on top

--- fetchchallenge/Views/MealListView.swift
+++ fetchchallenge/Views/MealListView.swift
@@ -19,12 +19,37 @@ struct MealListView: View {
                     Text("Error: \(errorMessage)")
                 } else {
                     List(viewModel.meals) { meal in
-                        HStack {
-                            AsyncImage(url: URL(string: meal.thumbnail))
-                                .frame(width: 50, height: 50)
-                                .clipShape(RoundedRectangle(cornerRadius: 8))
+                        ZStack {
+                            AsyncImage(url: URL(string: meal.thumbnail)) { phase in
+                                switch phase {
+                                case .empty:
+                                    Color.gray.opacity(0.3)
+                                case .success(let image):
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(height: 200)
+                                        .clipped()
+                                case .failure:
+                                    Color.red.opacity(0.3)
+                                @unknown default:
+                                    Color.gray.opacity(0.3)
+                                }
+                            }
+                            .frame(height: 200)
+                            .clipped()
+                            
                             Text(meal.name)
+                                .font(.headline)
+                                .fontWeight(.thin)
+                                .foregroundColor(.white)
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .cornerRadius(10)
+                                .padding(4)
                         }
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
                     }
                 }
             }
